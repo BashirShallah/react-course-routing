50bc77cb7ba2a316b20c71d9a7d5ee5471db08af
BashirShallah <bashir.shallah@gmail.com>
2019-02-09 13:00:18 +0300

    using withRouter HOC.

--- src/Components/ViewUser.js
+++ src/Components/ViewUser.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {withRouter} from "react-router-dom";
 
-export default function (props){
+const ViewUser = function (props){
     return <div>
+        Id: {props.match.params.id}
+
         <div>
             Name: {props.user.name}
         </div>
@@ -10,3 +13,5 @@ export default function (props){
         </div>
     </div>;
 }
+
+export default withRouter(ViewUser);
